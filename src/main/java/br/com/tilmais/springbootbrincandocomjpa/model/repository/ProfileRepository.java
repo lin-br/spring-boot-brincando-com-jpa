d2e7658acb7b4f46b2e887101af7c434c5f1bfc9
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-05-31 13:59:29 -0300

    refactor(ProfileRepository): Indetificar repositório com annotation

--- src/main/java/br/com/tilmais/springbootbrincandocomjpa/model/repository/ProfileRepository.java
+++ src/main/java/br/com/tilmais/springbootbrincandocomjpa/model/repository/ProfileRepository.java
@@ -2,6 +2,8 @@ package br.com.tilmais.springbootbrincandocomjpa.model.repository;
 
 import br.com.tilmais.springbootbrincandocomjpa.model.entity.Profile;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
 }
