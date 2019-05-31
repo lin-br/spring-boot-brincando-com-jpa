23ccf878b57cc5a2771c16e61a3ca0cb3632e643
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-05-31 12:10:05 -0300

    Corrigir annotation do ProfileController para @RestController

--- src/main/java/br/com/tilmais/springbootbrincandocomjpa/controller/ProfileController.java
+++ src/main/java/br/com/tilmais/springbootbrincandocomjpa/controller/ProfileController.java
@@ -5,14 +5,11 @@ import br.com.tilmais.springbootbrincandocomjpa.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("profiles")
 public class ProfileController {
 
