18a5b974970cbdb5875b07a1ef520018bd47d7ea
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-06-05 09:31:46 -0300

    Anotar classe com @Repository para ser gerado um Bean do Spring

--- src/main/java/br/com/tilmais/springbootbrincandocomjpa/model/repository/UserHasRulesRepository.java
+++ src/main/java/br/com/tilmais/springbootbrincandocomjpa/model/repository/UserHasRulesRepository.java
@@ -3,6 +3,8 @@ package br.com.tilmais.springbootbrincandocomjpa.model.repository;
 import br.com.tilmais.springbootbrincandocomjpa.model.entity.PksUsersHasRules;
 import br.com.tilmais.springbootbrincandocomjpa.model.entity.UsersHasRules;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserHasRulesRepository extends JpaRepository<UsersHasRules, PksUsersHasRules> {
 }
